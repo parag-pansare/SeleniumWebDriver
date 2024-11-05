7517d978a8e6a89e7fec28dce6cc6c82f370be86
parag.pansare <parag.pansare@PUNITP375434L.ad.infosys.com>
2024-11-06 00:20:56 +0530

    WebDriver methods demo

---
+++ src/test/java/day3/conditionalMethods.java
@@ -0,0 +1,31 @@
+package day3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class conditionalMethods {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.nopcommerce.com/");
+		driver.manage().window().maximize();
+		
+		WebElement logo=driver.findElement(By.xpath("//img[@alt=\"nopCommerce demo store\"]"));
+		System.out.println("Display status of logo is : " + logo.isDisplayed());
+		
+		Boolean searchBoxEnabled = driver.findElement(By.xpath("//input[@id= \"small-searchterms\"]")).isEnabled();
+		System.out.println("Search box enable status is : "+ searchBoxEnabled);
+		
+		driver.findElement(By.xpath("//a[@class=\"ico-register\"]")).click();
+		
+		WebElement female= driver.findElement(By.xpath("//input[@id=\"gender-female\"]"));
+		WebElement male= driver.findElement(By.xpath("//input[@id=\"gender-male\"]"));
+		System.out.println("Radio button selection status for female is : " + female.isSelected());
+		System.out.println("Radio button selection status for male is : " + male.isSelected());
+		female.click();
+		System.out.println("Radio button selection status for female is : " + female.isSelected());
+	}
+
+}
---
+++ src/test/java/day3/getMethods.java
@@ -0,0 +1,30 @@
+package day3;
+
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class getMethods {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		Thread.sleep(5000);
+		System.out.println("Title of page is : "+ driver.getTitle());
+		
+		System.out.println("URL of page is : " + driver.getCurrentUrl());
+		//System.out.println("WindowHandle of page is : " + driver.getWindowHandle());
+		//System.out.println("PageSource is : " + driver.getPageSource());
+		
+		driver.findElement(By.partialLinkText("OrangeHRM")).click();
+		Set<String> windowIds = driver.getWindowHandles();
+		System.out.println(windowIds);
+		Thread.sleep(5000);
+		driver.quit();
+	}
+
+}
