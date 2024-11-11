07ee1da586eb8cd7f48fe08c6f796e4c2909e7ff
parag.pansare <parag.pansare@PUNITP375434L.ad.infosys.com>
2024-11-11 17:12:05 +0530

    Day 6 handle checkboxes

---
+++ src/test/java/day6/handleCheckbox.java
@@ -0,0 +1,46 @@
+package day6;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class handleCheckbox {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();	
+		driver.get("https://trytestingthis.netlify.app/?fname=&lname=&gender=male&option=option&Optionwithcheck%5B%5D=option+1&Options=&favcolor=%2300ced1&day=&a=50&myfile=&quantity=5&message=The+cat+was+pl");
+		driver.manage().window().maximize();
+		
+		//driver.findElement(By.xpath("//input[@id='moption'][@name='option1']")).click();
+		
+		//select all checkboxes
+		List<WebElement> checkboxes = driver.findElements(By.xpath("//input[@id='moption' and @type='checkbox']"));
+		
+		/*for (int i=0; i<checkboxes.size(); i++) {
+			checkboxes.get(i).click();
+		}
+		*/
+		
+		/*for(WebElement cb:checkboxes) {
+			cb.click();
+		}
+		*/
+		
+		for (int i=0; i<2; i++) {
+			checkboxes.get(i).click();
+		}
+		
+		Thread.sleep(4000);
+		
+		for (int i=0; i<checkboxes.size(); i++) {
+			if(checkboxes.get(i).isSelected()) {
+			checkboxes.get(i).click();
+			}
+		}
+		
+	}
+
+}
